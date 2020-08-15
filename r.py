7a4f2789719ca3c3ecb1833660863b4a710f051e
James Zhang <Jameszhang9795478@gmail.com>
2020-08-15 02:37:09 -0400

    final updates

--- r.py
+++ r.py
@@ -1,12 +1,19 @@
 import random 
+y = input('please enter your starting number: ')
+e = input('please enter your ending number: ')
+y = int(y)
+e = int(e)
+r = random. randint(y, e)
+z = 0
 while True:
-	r = random. randint(1, 5)
+	z += 1 
 	x = input('guess the number: ')
 	x = int(x)
 	if x == r:
 		print('you guess the right number')
 		break
-	if x < r:
+	elif x < r:
 		print('smaller then answer')
 	elif x > r:
 		print('greater then aswer')
+	print('this is your' , z, 'times guesing')
